9608744e357ac14f1506f9f7c06f2c7bc3caa554
osmanx522 <osmanfadil52@gmail.com>
2026-04-14 23:35:08 +0300

    Implement MapMotor class with zoom functionality and add map image

--- gui.py
+++ gui.py
@@ -1,12 +1,46 @@
 import sys
+import os
 
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QApplication
+from PySide6.QtCore import Qt, QUrl
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QApplication, QGraphicsView, QGraphicsScene, QGraphicsPixmapItem
+from PySide6.QtGui import QPixmap, QPainter
 
+class MapMotor(QGraphicsView):
+    def __init__(self):
+        super().__init__()
+        self.toplam_zoom = 1.0
+        self.sahne = QGraphicsScene(self)
+        
+
+        self.setScene(self.sahne)
+
+        self.map_image = QPixmap("map.png")
+
+        self.harita_ogesi = QGraphicsPixmapItem(self.map_image)
+        self.sahne.addItem(self.harita_ogesi)
+        self.fitInView(self.harita_ogesi, Qt.KeepAspectRatio)
+
+        self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
+        self.setResizeAnchor(QGraphicsView.AnchorUnderMouse)
+    
+        self.setDragMode(QGraphicsView.ScrollHandDrag)
+        self.setRenderHint(QPainter.Antialiasing)
+        self.setRenderHint(QPainter.SmoothPixmapTransform)
+    
+    def wheelEvent(self, event):
+        if event.angleDelta().y() > 0:
+            if self.toplam_zoom < 8:
+               self.toplam_zoom *= 1.15
+               self.scale(1.15, 1.15)
+        else:
+            if self.toplam_zoom > 1.3:
+                self.toplam_zoom *= 0.85
+                self.scale(0.85, 0.85)
+        
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-
+        self.mapMotor = MapMotor()
         self.mainLayout = QVBoxLayout(self)
 
         self.weatherPanel= QHBoxLayout()
@@ -17,9 +51,10 @@ class MainWindow(QWidget):
         self.weatherPanel.addWidget(self.weatherinfo)
         self.weatherPanel.addWidget(self.weathertemperature)
         self.weatherPanel.addWidget(self.location)
-
+    
         self.mainLayout.addLayout(self.weatherPanel)
-
+        self.mainLayout.addWidget(self.mapMotor)
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
