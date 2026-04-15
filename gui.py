264ac3f34ecb0483216f83944182bdc7a296ad98
osmanx522 <osmanfadil52@gmail.com>
2026-04-16 02:33:24 +0300

    Refactor GUI to integrate DataWorker for real-time data processing and update graph display

--- gui.py
+++ gui.py
@@ -1,65 +1,101 @@
 import sys
-import os
+import pickle
+import pandas as pd
+import numpy as np
+import pyqtgraph as pg
+import itertools
+import sklearn
 
-from PySide6.QtCore import Qt, QUrl
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QApplication, QGraphicsView, QGraphicsScene, QGraphicsPixmapItem
-from PySide6.QtGui import QPixmap, QPainter
+from tensorflow.keras.models import load_model
+from collections import deque
+from PyQt6.QtCore import *
+from PyQt6.QtWidgets import *
 
-class MapMotor(QGraphicsView):
+class DataWorker(QObject):
+    data_ready = pyqtSignal(float, list)
+    def __init__(self, model, scaler, csv_path):
+        super().__init__()
+        self.model = model
+        self.scaler = scaler
+        self.data_buffer = deque(maxlen=30)
+        df = pd.read_csv(csv_path)
+        self.data_iterator = itertools.cycle(df.iloc[:, :8].values)
+
+    @pyqtSlot()
+    def process_data(self):
+        new_row = next(self.data_iterator)
+        self.data_buffer.append(new_row)
+        if len(self.data_buffer) == 30:
+            data_array = np.array(list(self.data_buffer))
+            normalized_data = self.scaler.transform(data_array)
+            normalized_tensor = normalized_data.reshape(1,30,8)
+            prediction = self.model.predict(normalized_tensor, verbose = 0)
+            score = float(prediction[0][0])
+
+            self.data_ready.emit(score, list(new_row))
+
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.toplam_zoom = 1.0
-        self.sahne = QGraphicsScene(self)
+        self.setWindowTitle("Model")
+        self.resize(800,600)
         
+        self.load_model_scaler()
+        
+        main_widget = QWidget(self)
+        main_layout = QVBoxLayout()
 
-        self.setScene(self.sahne)
+        pg.setConfigOptions(antialias=True)
+        self.graph = pg.PlotWidget()
+        self.graph.setBackground('#1e1e2f')
+        self.graph.showGrid(x=True, y=True, alpha=0.3)
+        self.graph.setLabel('left', 'Sensör Değeri')
+        self.graph.setLabel('bottom', 'Zaman Çizelgesi')
+        
+        kalem = pg.mkPen(color=(0, 255, 255), width=3)
+        self.starter_data = [0] * 1000
+        self.cizgi = self.graph.plot(self.starter_data, pen=kalem)
+        self.cizgi.setFillLevel(0)
+        self.cizgi.setBrush(pg.mkBrush(color=(0, 255, 255, 50)))
 
-        self.map_image = QPixmap("map.png")
+        main_layout.addWidget(self.graph)
+        main_widget.setLayout(main_layout)
+        self.setCentralWidget(main_widget)
 
-        self.harita_ogesi = QGraphicsPixmapItem(self.map_image)
-        self.sahne.addItem(self.harita_ogesi)
-        self.fitInView(self.harita_ogesi, Qt.KeepAspectRatio)
+        self.Thread()
 
-        self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
-        self.setResizeAnchor(QGraphicsView.AnchorUnderMouse)
-    
-        self.setDragMode(QGraphicsView.ScrollHandDrag)
-        self.setRenderHint(QPainter.Antialiasing)
-        self.setRenderHint(QPainter.SmoothPixmapTransform)
-    
-    def wheelEvent(self, event):
-        if event.angleDelta().y() > 0:
-            if self.toplam_zoom < 8:
-               self.toplam_zoom *= 1.15
-               self.scale(1.15, 1.15)
+    def Thread(self):
+        #Thread
+        self.data_worker = DataWorker(model=self.ai_model, scaler=self.ai_scaler, csv_path="model\\test_senaryosu.csv")
+        self.kanal = QThread()
+        
+        self.data_worker.moveToThread(self.kanal)
+        self.timer = QTimer()
+        self.timer.setInterval(1)
+        self.timer.moveToThread(self.kanal)
+        self.timer.timeout.connect(self.data_worker.process_data)
+
+        self.data_worker.data_ready.connect(self.graph_update)
+
+        self.kanal.started.connect(self.timer.start)
+        self.kanal.start()
+
+    def graph_update(self, oran, liste):
+        self.starter_data.pop(0)
+        self.starter_data.append(liste[0])
+        self.cizgi.setData(self.starter_data)
+        if oran > 0.5:
+            self.setWindowTitle(f"UYARI: SALDIRI TESPİT EDİLDİ! (%{oran*100:.2f})")
         else:
-            if self.toplam_zoom > 1.3:
-                self.toplam_zoom *= 0.85
-                self.scale(0.85, 0.85)
-        
-class MainWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.mapMotor = MapMotor()
-        self.mainLayout = QVBoxLayout(self)
+            self.setWindowTitle(f"Durum Normal (%{oran*100:.2f})")
 
-        self.weatherPanel= QHBoxLayout()
-        self.weatherinfo = QLabel("Hava Durumu ☀️: ")
-        self.weathertemperature = QLabel("Hava Sıcaklığı 🌡️: ")
-        self.location = QLabel("Konum Bilgisi [x: y: z:]")
+    def load_model_scaler(self):
+        self.ai_model = load_model("model\\crnn_model.h5")
+        with open("model\\crnn_scaler.pkl", "rb") as f:
+            self.ai_scaler = pickle.load(f)
 
-        self.weatherPanel.addWidget(self.weatherinfo)
-        self.weatherPanel.addWidget(self.weathertemperature)
-        self.weatherPanel.addWidget(self.location)
-    
-        self.mainLayout.addLayout(self.weatherPanel)
-        self.mainLayout.addWidget(self.mapMotor)
-        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
-    window.resize(800,600)
-    window.setWindowTitle("EFES Project GUI")
     window.show()
-
     sys.exit(app.exec())
